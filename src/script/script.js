436253a6c3c5103235122e94a41123500dee646a
jonesnambunndo@hotmail.com <jonesnambundo@mtel.inatel.br>
2024-03-12 09:43:31 -0300

    Commit inicial

---
+++ src/script/script.js
@@ -0,0 +1,21 @@
+function toggleVideo() {
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    video.pause();
+    trailer.classList.toggle('active');
+}
+
+function changeBg(bg, title) {
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelectorAll('.content');
+    banner.style.background = `url("/src/images/${bg}")`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+
+    contents.forEach(content => {
+        content.classList.remove('active');
+        if (content.classList.contains(title)) {
+            content.classList.add('active');
+        }
+    });
+}
